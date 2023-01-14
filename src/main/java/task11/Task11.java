6e37851af112029929ed7993726b03a9bebdf05b
Jelena Jakobson <lena.docha@gmail.com>
2023-01-14 20:49:56 +0200

    add: task 11 and 13

--- src/main/java/task11/Task11.java
+++ src/main/java/task11/Task11.java
@@ -1,6 +1,26 @@
 package task11;
+
+import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
+
 /*11***. Дан массив интов. Написать метод в который вы передаете массив и еще одно число.
 Метод должен ВЕРНУТЬ ИНДЕКС ДВУХ ЧИСЕЛ СУММА КОТОРЫХ ДАЕТ ЭТО ЧИСЛО.
 ЕСЛИ ТАКОВЫХ НЕТ - СООБЩЕНИЕ.*/
 public class Task11 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4,5,6,7,99};
+        int target = 99;
+        System.out.println(findTwoNumbers(arr, target));
+    }
+
+    public static String findTwoNumbers(int[] arr, int target) {
+        return IntStream.range(0, arr.length)
+                .boxed()
+                .flatMap(i -> IntStream.range(i + 1, arr.length)
+                        .filter(j -> arr[i] + arr[j] == target)
+                        .mapToObj(j -> String.format("Indexes of two numbers are: %d, %d", i, j))
+                )
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("no such elements"));
+    }
 }
--- src/main/java/task13/Task13.java
+++ src/main/java/task13/Task13.java
@@ -2,9 +2,11 @@ package task13;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /*13**Написать метод
 input -> int a =221
@@ -16,13 +18,21 @@ out -> -321
 !!Помните по диапазон интов!!*/
 public class Task13 {
     public static void main(String[] args) {
-        int a = 221;
+        int a = 221419958;
         int b = -123;
-        IntStream.of(b)
-                .boxed()
-                .collect(Collectors.toList())
-                .stream()
-                .sorted(Collections.reverseOrder())
-                .forEach(System.out::println);
+
+        System.out.println(reverseInt(a));
+        System.out.println();
+        System.out.println(reverseInt(b));
+    }
+
+    private static String reverseInt(int num) {
+        String s = String.valueOf(Integer.toString(Math.abs(num)).chars()
+                .map(Character::getNumericValue)
+                .mapToObj(Integer::toString)
+                .reduce((i, j) -> j+i)
+                .map(s1 -> num < 0 ? "-" + s1 : s1)
+                .orElse(""));
+        return s;
     }
 }
